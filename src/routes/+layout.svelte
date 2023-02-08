abd680256b0ad52fa6f7a9f74e784cfb4452935d
enchart <77974051+enchart@users.noreply.github.com>
2023-02-08 21:06:26 +0500

    Added normalize.css and Comic Neue font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.scss";
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+  <link href="https://fonts.googleapis.com/css2?family=Comic+Neue&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+</svelte:head>
+
+<slot />
