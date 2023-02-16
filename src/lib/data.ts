ce901b53f8d862ce66cb7f9413411fafa4f6fc0c
enchart <77974051+enchart@users.noreply.github.com>
2023-02-16 19:15:02 +0500

    Added stop-bullying

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,5 @@
 export const headerLinks: Record<string, string | string[]> = {
-	'Белые сайты': ['/white-sites', '/white-sites/search']
-	// '/stop-bullying': 'Стоп-буллинг',
+	'Белые сайты': ['/white-sites', '/white-sites/search'],
+	'Стоп-буллинг': '/stop-bullying'
 	// '/test': 'Тест'
 };
