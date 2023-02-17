5f7c9426fde628bf380a5cd295c558dd21dc786b
enchart <77974051+enchart@users.noreply.github.com>
2023-02-17 07:42:32 +0500

    Added meta to root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
 <svelte:head>
 	<title>сотсгум))</title>
+	<meta property="og:locale" content="ru_RU" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="сотсгум))" />
+	<meta property="og:description" content="Неофициальный сайт лучшего колледжа в ХМАО" />
+	<!-- <meta property="og:image" content="http://site.com/uploads/image_1.png" /> -->
+	<meta property="og:url" content="http://sotsgum.ru/" />
+	<meta property="og:site_name" content="сотсгум))" />
 </svelte:head>
