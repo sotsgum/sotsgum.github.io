2b93a9fa42796a1875826ed8376ece546f2cdadf
enchart <77974051+enchart@users.noreply.github.com>
2023-02-17 22:55:19 +0500

    Added banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,9 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="сотсгум))" />
 	<meta property="og:description" content="Неофициальный сайт лучшего колледжа в ХМАО" />
-	<!-- <meta property="og:image" content="http://site.com/uploads/image_1.png" /> -->
+	<meta property="og:image" content="http://sotsgum.ru/banner.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="627" />
 	<meta property="og:url" content="http://sotsgum.ru/" />
 	<meta property="og:site_name" content="сотсгум))" />
 </svelte:head>
