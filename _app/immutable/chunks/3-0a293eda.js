c40649b69b209bf748a2831a543c7a29ec7348bc
enchart <77974051+enchart@users.noreply.github.com>
2025-05-13 20:00:37 +0500

    Updates

--- _app/immutable/chunks/3-0a293eda.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/stop-bullying/_page.svelte-10b2483a.js";export{t as component};
--- _app/immutable/chunks/stores-7c181528.js
+++
@@ -1,1 +0,0 @@
-import"./index-9d02c8d6.js";import{s as e}from"./singletons-959e1805.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
